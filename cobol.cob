b6c34d62245ef66272e5d3e9b26f83be6e0131f3
Cardenas26 <118994173+Cardenas26@users.noreply.github.com>
2023-04-02 08:11:44 -0500

    Create cobol.cob

---
+++ cobol.cob
@@ -0,0 +1,15 @@
+      *****************************************************************
+      * Program name:    MYPROG                               
+      * Original author: MYNAME                                
+      *
+      * Maintenence Log                                              
+      * Date      Author        Maintenance Requirement               
+      * --------- ------------  --------------------------------------- 
+      * 01/01/08 MYNAME  Created for COBOL class         
+      *                                                               
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  MYPROG.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, world!".
+           STOP RUN.
